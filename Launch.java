30fdb6dc809ccad1e82ef9422c9bbd9c7a2eec64
namang48 <153305820+namang48@users.noreply.github.com>
2024-04-03 09:16:07 +0530

    First ever commit

---
+++ Launch.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Launch{
+	public static void main(String args[])
+		{
+			System.out.println("Hello Github !");
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter a Number : ");
+			int num=sc.nextInt();
+			System.out.println("Number is "+ num);
+		}
+}
