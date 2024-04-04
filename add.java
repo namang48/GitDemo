b68c85830a4d0bc09f7df575018eff371d39a10d
namang48 <namangupta.mp4@gmail.com>
2024-04-04 07:18:55 +0530

    pull practice

--- add.java
+++ add.java
@@ -2,4 +2,10 @@ class add{
 	public static void main(String args[]){
 		System.out.println("Hello, pull and clone practice");
 	}
+	public static void add(){
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
 }
